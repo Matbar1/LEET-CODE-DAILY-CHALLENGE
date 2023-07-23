325b7a8d94b6206508cf8bbe9730243b4e179d50
Matbar1 <136784491+Matbar1@users.noreply.github.com>
2023-07-23 07:49:18 +0530

    Add files via upload

---
+++ 23.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<TreeNode*> allPossibleFBT(int N) {
+        if (N % 2 == 0) return {};
+        if (N == 1) return { new TreeNode(0) };
+        vector<TreeNode*> ans;
+        for (int i = 1; i <= N - 2; i += 2) {
+            auto lefts = allPossibleFBT(i);
+            auto rights = allPossibleFBT(N - i - 1);
+            for (auto left : lefts) {
+                for (auto right : rights) {
+                    auto root = new TreeNode(0);
+                    root->left = left;
+                    root->right = right;
+                    ans.push_back(root);
+                }
+            }
+        }
+        return ans;
+    }
+};
