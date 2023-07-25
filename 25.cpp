5ae0de78fd526ba987c425fa2a09d5fa8bf9296e
Matbar1 <136784491+Matbar1@users.noreply.github.com>
2023-07-25 17:45:53 +0530

    Add files via upload

---
+++ 25.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        int i = 1;
+        while (i < A.size() && A[i] > A[i - 1]) ++i;
+        return i - 1;
+    }
+};
