049ca8045b16637ddf60b94216993db7fb564949
Matbar1 <136784491+Matbar1@users.noreply.github.com>
2023-08-03 09:09:16 +0530

    Add files via upload

--- letter combination of a phone no.cpp
+++ letter combination of a phone no.cpp
@@ -1,49 +1,22 @@
-class Solution {
-  public:
-     void findTopoSort(int node, int vis[], stack<int> &st, vector<pair<int, int>> adj[]) {
-        vis[node] = 1;
-        for (auto it : adj[node]) {
-            if (!vis[it.first]) {
-                findTopoSort(it.first, vis, st, adj);
-            }
+class Solution { 
+  vector<string> m{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> ans;
+    void dfs(string &digits, int start, string &str) {
+        if (start == digits.size()) {
+            ans.push_back(str);
+            return;
         }
-        st.push(node);
+        for (char c : m[digits[start] - '2']) {
+            str.push_back(c);
+            dfs(digits, start + 1, str);
+            str.pop_back();
+        }
     }
-
-
-    vector<int> shortestPath(int N, int M, vector<vector<int>>& edges)
-    {
-        vector<pair<int, int>> adj[N];
-        int src = 0;
-        for (int i = 0; i < M; i++) {
-            adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
-        }
-        int vis[N] = {0};
-        stack<int> st;
-        for (int i = 0; i < N; i++)
-            if (!vis[i])
-                findTopoSort(i, vis, st, adj);
-
-        vector<int> dist(N, 1e9);
-        dist[src] = 0;
-
-        while (!st.empty())
-        {
-            int node = st.top();
-            st.pop();
-
-            if (dist[node] != 1e9) {
-                for (auto it : adj[node]) {
-                    if (dist[node] + it.second < dist[it.first]) {
-                        dist[it.first] = dist[node] + it.second;
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < dist.size(); ++i) {
-            if (dist[i] ==  1e9)dist[i] = -1;
-        }
-        return dist;
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        string str;
+        dfs(digits, 0, str);
+        return ans;
     }
 };
